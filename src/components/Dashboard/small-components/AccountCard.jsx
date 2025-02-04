65d01ece0747c4c8bcfb3b56c329a9ae9026b77d
Abubakar Sadiq <sadiqmuhammad2101@gmail.com>
2025-02-04 12:03:40 +0100

    Update Mobile AccountCard style

--- src/components/Dashboard/small-components/AccountCard.jsx
+++ src/components/Dashboard/small-components/AccountCard.jsx
@@ -15,14 +15,14 @@ const AccountCard = ({ className, bankIcon, bankName, accountName, accountNumber
     }
 
     return (
-        <div className={`relative p-4 flex max-lg:flex-col gap-5 lg:gap-4 lg:p-[10px] rounded-2xl ${className}`}>
+        <div className={`relative p-4 max-lg:p-2 flex max-lg:flex-col gap-5 lg:gap-4 lg:p-[10px] rounded-2xl ${className}`}>
             <div className={`rounded-full ${iconBack}`}>
-                <img src={bankIcon} />
+                <img src={bankIcon} className='max-lg:w-[40px] max-lg:h-[40px]' />
             </div>
             <div className='text-white'>
-                <h3 className='font-semibold lg:font-normal lg:text-sm mb-2'>{bankName}</h3>
+                <h3 className='font-semibold lg:font-normal lg:text-sm mb-2 max-lg:mb-1'>{bankName}</h3>
                 <p className='font-extralight lg:text-sm max-lg:text-[11px]'><spam className="max-lg:hidden">Name:</spam> <span className='font-semibold lg:font-normal'>{accountName}</span></p>
-                <p className='my-2 font-extralight lg:text-sm max-lg:text-[11px]'><span className="max-lg:hidden">Account Number:</span> <span className='font-semibold lg:font-normal'>{accountNumber}</span></p>
+                <p className='my-2 max-lg:my-1 font-extralight lg:text-sm max-lg:text-[11px]'><span className="max-lg:hidden">Account Number:</span> <span className='font-semibold lg:font-normal'>{accountNumber}</span></p>
                 <button onClick={handleCopy} className={`cursor-pointer flex gap-2 items-center mt-2 py-1 px-3 rounded-lg bg-[#FFE9F8] max-lg:absolute max-lg:right-4 max-lg:top-4 max-lg:rounded-full max-lg:p-2 max-lg:text-xl max-lg:bg-transparent ${copied ? 'text-green-500 max-lg:text-green max-lg:border-green' : 'text-orange-600 max-lg:text-red-900 max-lg:border max-lg:border-red-900'}`}>
                     <span>
                         {copied ? <IoCopy /> : <TbCopy />}
--- src/components/Dashboard/small-components/Accounts.jsx
+++ src/components/Dashboard/small-components/Accounts.jsx
@@ -23,9 +23,9 @@ const Accounts = () => {
                 <h2 className='text-xl text-gray-900'>Accounts</h2>
                 <button className='p-2 rounded-lg cursor-pointer text-gray-900 text-sm bg-blue-400' onClick={handleAccountModal}>Use Temporary account</button>
             </div>
-            <div className='m-3 max-lg:mx-0 lg:mx-0  grid grid-cols-2 lg:grid-cols-3 gap-3'>
+            <div className='m-3 max-lg:mx-0 lg:mx-0  grid grid-cols-2 lg:grid-cols-3 gap-3 max-lg:gap-1'>
                 <AccountCard className="bg-[#AE328E]" bankIcon={Wema} bankName="Wema Bank PLC" accountName="Jeff Grimes" accountNumber="0048932093" />
-                <AccountCard iconBack="w-[60px] h-[60px] rounded-full bg-gray-800 flex items-center justify-center" className="bg-[#F1454A]" bankIcon={Sterlling} bankName="Sterling Bank" accountName="Jeff Grimes" accountNumber="0048932093" />
+                <AccountCard iconBack="w-[60px] h-[60px] max-lg:w-[40px] max-lg:h-[40px] rounded-full bg-gray-800 flex items-center justify-center" className="bg-[#F1454A]" bankIcon={Sterlling} bankName="Sterling Bank" accountName="Jeff Grimes" accountNumber="0048932093" />
                 <AccountCard className="bg-[#031335]" bankIcon={Monie} bankName="Monie Point" accountName="Jeff Grimes" accountNumber="0048932093" />
             </div>
         </div>
