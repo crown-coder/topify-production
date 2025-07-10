53fb3dc2e467045965e8942c6d881ff08a32c0c9
crown-coder <75354991+crown-coder@users.noreply.github.com>
2025-07-10 17:24:31 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { useAuth } from '../hooks/useAuth'
 import Hero from '../components/Home/Hero'
 import WhyChooseUs from '../components/Home/WhyChooseUs'
 import AboutUs from '../components/Home/AboutUs'
@@ -8,13 +7,13 @@ import OurServices from '../components/Home/OurServices'
 import Pricing from '../components/Home/Pricing'
 import Footer from '../components/Home/Footer'
 import FAQ from '../components/Home/FAQ'
-import Demo from '../components/Home/Demo'
 
 const Home = () => {
+    useAuth({ middleware: 'guest', redirectIfAuthenticated: '/dashboard' })
+
     return (
         <div>
             <Hero />
-            {/* <Demo /> */}
             <WhyChooseUs />
             <AboutUs />
             <Testimonial />
@@ -22,11 +21,8 @@ const Home = () => {
             <Pricing />
             <FAQ />
             <Footer />
-            {/* <Link to="/dashboard">Go to User Dashboard</Link>
-            <Link to="/admin">Go to Admin Dashboard</Link> */}
         </div>
     )
 }
 
 export default Home
-Home
